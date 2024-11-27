a6f379ae3bf8efca070f1086faf912431a487bf6
Mohammad Shajalal <khanranu81@gmail.com>
2024-11-27 16:19:06 +0600

    tailwind config file customize and hero,section1, section2 create

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-primary': '#9D3971',
+        'color-secndary': '#161616',
+        'gradientStart': '#C27B9F',
+        'gradientEnd': '#901D63',
+        'color-border': '#F0F0F0',
+        'color-white-dark': '#FDF7FB',
+        'gradient-start': '#FFFFFF',
+        'gradient-end': '#D8A7BE',
+      }
+    },
+    container : {
+      center: true,
+      padding: {
+        DEFAULT: '40px',
+        md: '50px',
+      }
+    }
   },
   plugins: [],
 }
